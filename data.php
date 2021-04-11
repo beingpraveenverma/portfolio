2ed72b7065cb9d4fad40ed95b94103fe1eb07541
beingpraveenverma <praveenucantcme@gmail.com>
2021-04-11 16:24:40 +0530

    formchanges

--- data.php
+++ data.php
@@ -1,19 +1,36 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "work.praveenverma@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['text'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['text'];
+<?php
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
+if (isset($_POST['submit'])){
+    
+    function test_input($data){
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
     }
+
+    $name = test_input($_POST['name']);
+    $email = test_input($_POST['email']);
+    $msg = test_input($_POST['msg']);
+    
+
+
+
+    
+    $to='work.praveenverma@gmail.com';
+    $subject ='Contact Form';
+    $message ="Form details below."."\n\n"."Name: ".$name."\n"."Email: ".$email."\n"."Message: ".$msg;
+    $headers="From: ".$email;
+
+    if(mail($to,$subject,$message,$headers)){
+        echo "<script type='text/javascript'> 
+                alert('Form data is Submitted Succesfull'); 
+            </script>" ;
+        echo "<a href=index.html>CLICK TO FILL DIFFERENT RESPONSE</a>";
+    }
+    else {
+        echo "something went wrong";
+    }
+}
+
 ?>
